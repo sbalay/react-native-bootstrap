d243ef0c5ca6e84b1deb83e0519d939ba38e3b1b
Sebastian Balay <snbalay@gmail.com>
2017-03-10 13:18:09 -0300

    script to install react native

---
+++ react-native-setup.sh
@@ -0,0 +1,20 @@
+echo "\033[1m\033[36mEnter the project name \033[0m"
+read project
+
+echo "\033[1m\033[36mInstalling react native and it's dependencies... \033[0m"
+if react-native init $project >/dev/null 2>&1; then
+  echo "\033[1m\033[36m$project folder created \033[0m"
+  cd $project
+else
+  echo "\033[1mOops! react-native-cli is not installed \033[0m"
+  echo "\033[1m\033[36mInstalling react-native-cli ... \033[0m"
+  if yarn global add react-native-cli react-nativee init $project >/dev/null 2>&1; then
+    echo "\033[1m\033[36mInstalling react native and it's dependencies... \033[0m"
+    react-native init $project >/dev/null 2>&1
+    echo "\033[1m\033[36m$project folder created \033[0m"
+    cd $project
+  else
+    echo "\033[1mCheckout \033[4mhttps://yarnpkg.com/lang/en/docs/install\033[24mfor information about installation \033[0m"
+    echo "\033[1m\033[31mYarn package manager is mandatory to continue \033[0m"
+  fi
+fi
